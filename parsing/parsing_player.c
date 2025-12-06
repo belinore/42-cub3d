13e5823306ce79ecf6344f2dece21bb903c6b6eb
jmehmy42 <jmehmy@student.42lisboa.com>
2025-12-06 16:45:33 +0000

    remove redundant player position setup

--- parsing/parsing_player.c
+++ parsing/parsing_player.c
@@ -12,42 +12,6 @@
 
 #include "cub3d.h"
 
-static void	orientation_east_or_west(t_game *game, char c)
-{
-	if (c == 'E')
-	{
-		game->player.dir_x = 1;
-		game->player.dir_y = 0;
-		game->player.plane_x = 0;
-		game->player.plane_y = 0.66;
-	}
-	else if (c == 'W')
-	{
-		game->player.dir_x = -1;
-		game->player.dir_y = 0;
-		game->player.plane_x = 0;
-		game->player.plane_y = -0.66;
-	}
-}
-
-static void	orientation_north_or_south(t_game *game, char c)
-{
-	if (c == 'N')
-	{
-		game->player.dir_x = 0;
-		game->player.dir_y = -1;
-		game->player.plane_x = 0.66;
-		game->player.plane_y = 0;
-	}
-	else if (c == 'S')
-	{
-		game->player.dir_x = 0;
-		game->player.dir_y = 1;
-		game->player.plane_x = -0.66;
-		game->player.plane_y = 0;
-	}
-}
-
 static void	process_cell_player(t_game *game, int i, int j, int *count)
 {
 	char	c;
@@ -56,12 +20,6 @@ static void	process_cell_player(t_game *game, int i, int j, int *count)
 	if (c != 'N' && c != 'S' && c != 'E' && c != 'W')
 		return ;
 	(*count)++;
-	game->player.pos_x = (double)j + 0.5;
-	game->player.pos_y = (double)i + 0.5;
-	if (c == 'N' || c == 'S')
-		orientation_north_or_south(game, c);
-	if (c == 'E' || c == 'W')
-		orientation_east_or_west(game, c);
 }
 
 int	validate_and_store_player(t_game *game)
